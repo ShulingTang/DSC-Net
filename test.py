33b4b5da0f22b4c4e52bef1f2cb3b352b833e2ae
ShulingTang <tangshuling@nudt.edu.cn>
2020-06-10 15:49:32 +0800

    这部分写了一个函数，用来生成系数矩阵Ｃ，但是在处理qp问题时，会出现错误。

--- test.py
+++ test.py
@@ -1,21 +1,42 @@
+from sklearn.cluster import KMeans
 import numpy as np
+from cvxopt import matrix, solvers
 
-
-def spectual_clustering(A):
-
-    print('A', A)
-    print('A.shape', A.shape)
-    D = np.sum(A, axis=1)
-    print('D', D)
-    D = D[0:3]
-    # D =D.reshape(A.shape[0])
-    print('D', D)
-    D = np.diag(D)
-    print('D', D)
-
-    return D
+def get_Coefficient(X, kmeansNum, alpha):
+    """
+    In this part, first conduct kmeans to obtain M anchor points,
+    and then construct a coefficient matrix C through the anchor points
+    and all the data.
+    :param X: sampled data
+    :param M:anchor graph
+    :return C:Coefficient Matrix
+    """
+    # X.shape = (n, d), M.shape = (m, d)
+    M = KMeans(n_clusters=kmeansNum, random_state=0).fit(X)
+    num, dim = X.shape
+    M = M.cluster_centers_
+    A = 2*alpha*np.identity(kmeansNum)+2*np.matmul(M, M.T)
+    A = matrix(1/2*(A+A.T))  # A.shape = (m, m)
+    B = X.T  # B.shape = (d, n)
+    Z = []
+    I = matrix(np.ones(kmeansNum))
+    # I = matrix(I.T)
+    print(I.shape)
+    b = matrix(0.0)
+    for i in range(num):
+        # fi.shape = (m ,1)
+        fi = np.matmul(-2*(B[:, i]).T, M.T)
+        fi = matrix(fi.reshape(1, kmeansNum))
+        zi = solvers.qp(A, fi, G=None, h=None, A=I, b=b, kktsolver=None)
+        Z = np.r_[Z, zi]
+    return Z
 
 
 if __name__ == "__main__":
-    A = np.arange(1, 13).reshape(4, 3)
-    spectual_clustering(A)
+    C = np.random.randint(1, 10, (1000, 216))
+    # 聚类个数
+    K = 200
+    # 子空间维数
+
+    alpha = 1
+    y = get_Coefficient(C, K, alpha)
