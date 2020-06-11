4ee13640a664ac84ac9c951403793e355c3a6b71
ShulingTang <tangshuling@nudt.edu.cn>
2020-06-11 20:41:34 +0800

    将LMVSC的svd部分代码，迁移过来，用于得到系数矩阵之后获取划分U，再进行KMEANS得到聚类结果

--- post_clustering.py
+++ post_clustering.py
@@ -1,8 +1,13 @@
 import numpy as np
-from scipy.sparse.linalg import svds
+from numpy import *
+from scipy.sparse.linalg import svds, eigs
 from sklearn.preprocessing import normalize
 from sklearn.metrics import normalized_mutual_info_score, adjusted_rand_score, adjusted_mutual_info_score
 from sklearn.cluster import KMeans
+from scipy.sparse import spdiags
+# import scipy as sp
+# import scipy.sparse.linalg
+# import scipy.io as sio
 
 nmi = normalized_mutual_info_score
 ami = adjusted_mutual_info_score
@@ -53,11 +58,25 @@ def post_proC(C, K, d, alpha):
     return y, U
 
 def spectral_clustering(C, K, d, alpha, ro):
-    y, _ = post_proC(C, K, d, ro)
+    # y, _ = post_proC(C, K, d, ro)
+    y, _ = mysvd(C, K)
     return y
 
+####################################
 
+def sort_eigvector_by_eigvalue(a, b):
+    '''
 
+    :param a: eigvalue
+    :param b: eigvector
+    :return:
+    '''
+    a = -np.abs(a)
+    asort = np.abs(np.sort(a, axis=0))
+    index = a.argsort()
+    bsort = b[:, index]
+
+    return asort, bsort
 def matlab_max(A, B):
     shape = A.shape
     a = A.ravel()
@@ -76,18 +95,87 @@ def mysvd(C, ReducedDim=None):
     max_matrix_size = 1600
     eigvector_ratio = 0.1
 
-    if not ReducedDim is None:
-        k = 0
+    if ReducedDim is None:
+        ReducedDim = 0
     nSmp, mFea = C.shape
     if mFea/nSmp > 1.0713:
         ddata = np.matmul(C, C.T)
         ddata = matlab_max(ddata, ddata.T)
 
-        dimMatric = ddata.shape(1)
+        dimMatric = ddata.shape[0]
         if ReducedDim > 0 and dimMatric > max_matrix_size and ReducedDim < dimMatric*eigvector_ratio:
+            # option = {"disp": [0]}
+            # eigvalue, U = np.linalg.eig(ddata)
+            U, eigvalue = eigs(ddata, ReducedDim, which='LM')
+            eigvalue = np.diag(eigvalue)
+        else:
+            '''
+            # matlab code
+            if issparse(ddata)
+                ddate = full(ddata)
+                
+            '''
+            eigvalue, U = np.linalg.eig(ddata)
+            # eigvalue = np.diag(eigvalue)
+            # eigvalue = np.abs(np.sort(-np.abs(eigvalue), axis=0))
+            eigvalue, U = sort_eigvector_by_eigvalue(eigvalue, U)
+        maxeigvalue = np.amax(eigvalue, axis=0)
+        eigIdx = np.argwhere(abs(eigvalue)/maxeigvalue<1e-10)
+        eigvalue[:, eigIdx] = []
+        U[:, eigIdx] = []
+        if ReducedDim > 0 and ReducedDim < len(eigvalue):
+            eigvalue = eigvalue[0:ReducedDim]
+            U = U[:, 0:ReducedDim]
+        eigvalue_half = eigvalue**(0.5)
+        S = spdiags(eigvalue_half, 0, len(eigvalue_half), len(eigvalue_half))
+        nargout = 3  # Number of function outputs
+        if nargout >= 3:
+            eigvalue_minushalf = eigvalue_half**(-1)
+            V = np.matmul(C.T, np.multiply(U, np.tile(eigvalue_minushalf.T, U.shape[0], 1)))
+    else:
+        ddata = np.matmul(C.T, C)
+        ddata = matlab_max(ddata, ddata.T)
+        dimMatric = ddata.shape[0]
+
+        if ReducedDim > 0 and dimMatric > max_matrix_size and ReducedDim < dimMatric*eigvector_ratio:
+            V, eigvalue = eigs(ddata, ReducedDim, which='LM')
+            eigvalue = np.diag(eigvalue)
+        else:
+            '''
+            # matlab code
+            if issparse(ddata)
+                ddate = full(ddata)
+
+            '''
+            eigvalue, V = np.linalg.eig(ddata)
+            # eigvalue = np.diag(eigvalue)
+            # eigvalue = np.abs(np.sort(-np.abs(eigvalue), axis=0))
+            eigvalue, V = sort_eigvector_by_eigvalue(eigvalue, V)
+        maxeigvalue = np.amax(eigvalue, axis=0)
+        evaluate = maxeigvalue*(1e-10)
+        eigIdx = np.argwhere(abs(eigvalue) < evaluate)
+        # print('eigIdx:', eigIdx)
+        # print('eigvalue:', eigvalue)
+        #########
+        eigvalue = delete(eigvalue, eigIdx)
+        V = delete(V, eigIdx, axis=1)
+        # eigvalue[:, eigIdx] = []
+        # V[:, eigIdx] = []
+        if ReducedDim > 0 and ReducedDim < len(eigvalue):
+            eigvalue = eigvalue[0:ReducedDim]
+            V = V[:, 0:ReducedDim]
+        eigvalue_half = eigvalue ** (0.5)
+        S = spdiags(eigvalue_half, 0, len(eigvalue_half), len(eigvalue_half))
+        eigvalue_minushalf = eigvalue_half**(-1)
+        U = np.matmul(C, np.multiply(V, np.tile(eigvalue_minushalf.T, (V.shape[0], 1))))
+        y = KMeans(n_clusters=ReducedDim, random_state=0).fit(U)
+        y = y.labels_
+
+    return y, U
 
 
-
-
-
-
+if __name__ == "__main__":
+    C = np.random.randint(1, 10, (1000, 216))
+    k = 20
+    U = mysvd(C, k)
+    print('U:', U)
